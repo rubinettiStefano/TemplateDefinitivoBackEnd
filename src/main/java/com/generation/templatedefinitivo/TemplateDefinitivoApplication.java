778c3602f9dd6144ae95ffd4d51daa40f41f9d80
StefanoR <rubinetti.stefano@gmail.com>
2025-09-29 10:34:13 +0200

    Initial commit

---
+++ src/main/java/com/generation/templatedefinitivo/TemplateDefinitivoApplication.java
@@ -0,0 +1,13 @@
+package com.generation.templatedefinitivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateDefinitivoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateDefinitivoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/generation/templatedefinitivo/TemplateDefinitivoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.generation.templatedefinitivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SpringBootTest
+class TemplateDefinitivoApplicationTests {
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
